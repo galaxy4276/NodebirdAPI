3ddb2e2debc2324140cc6b4b3eb66697be010f87
galaxy4276 <galaxyhi4276@gmail.com>
2020-04-17 13:08:58 +0900

    Domain Schema 작성

--- app.js
+++ app.js
@@ -53,12 +53,14 @@ app.use((req, res, next) => {
   next(err);
 });
 
+
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
   res.render('error');
-})
+});
+
 
 app.listen(app.get('port'), () => {
   console.log(`${app.get('port')} 번 포트에서 대기 중..`);
